f60ad7b02161c1a598afee4e87c127c673a76d69
Nithiya Streethran <nmstreethran@gmail.com>
2023-11-15 17:33:10 +0000

    reorganise

--- docs/convert-notebooks.sh
+++ docs/convert-notebooks.sh
@@ -1,22 +1,20 @@
-jupyter nbconvert --sanitize-html --to notebook --inplace docs/*.ipynb
+jupyter nbconvert --sanitize-html --to notebook --inplace docs/python/*.ipynb
 
 # convert Jupyter Notebooks to Python scripts
-jupyter nbconvert --to script docs/bokeh*.ipynb
-jupyter nbconvert --to script docs/geopandas*.ipynb
-jupyter nbconvert --to script docs/rioxarray*.ipynb
+jupyter nbconvert --to script docs/python/*.ipynb
 
 # remove "# In []" and multiple blank lines
-for f in docs/*.py;
+for f in docs/python/*.py;
 do sed -i -e '/^# In\[/d' $f
 cat -s $f > $f.txt
 mv $f.txt $f
 done
 
 # format scripts
-black -l 79 docs/*.py
+black -l 79 docs/python/*.py
 
 # sort imports
-isort docs/*.py
+isort docs/python/*.py
 
 # move to a different directory
-mv docs/*.py charts/python/
+mv docs/python/*.py charts/python/
